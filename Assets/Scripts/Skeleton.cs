92d95b3979b3a34d1b91413ad3e12f8e1ae8b76b
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2024-03-21 09:08:37 +1300

    Added prefab for Skeleton

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
