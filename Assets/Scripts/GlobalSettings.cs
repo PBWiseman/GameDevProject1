adda951d8c7ad60ebc453d038d8388347a18c0af
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2024-03-28 09:58:30 +1300

    In progress fixing player loading

---
+++ Assets/Scripts/GlobalSettings.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalSettings
+{
+    //Global loaded player variable
+}
--- Assets/Scripts/LoadPlayer.cs
+++ Assets/Scripts/LoadPlayer.cs
@@ -6,12 +6,25 @@ using UnityEngine.SceneManagement;
 public class LoadPlayer : MonoBehaviour
 {
     //If the player scene is not loaded then load it. This is on all scenes in case a scene is being started from the editor and not from the main menu
-    void Awake()
+    void Start()
     {
         //if player scene is not loaded then load it
-        if (SceneManager.GetSceneByName("Player").isLoaded == false)
-        {
-            SceneManager.LoadScene("Player", LoadSceneMode.Additive);
-        }
+        // int count = SceneManager.sceneCount;
+        // Debug.Log(count);
+        // for (int i = 0; i < count; i++)
+        // {
+        //     Scene scene = SceneManager.GetSceneAt(i);
+        //     Debug.Log(scene.name);
+        //     if (scene.name == "Player")
+        //     {
+        //         return;
+        //     }
+        // }
+        
+
+        // if (SceneManager.GetSceneByName("Player").isLoaded == false)
+        // {
+        //     SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+        // }
     }
 }
